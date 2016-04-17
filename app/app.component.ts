38aa89ffb4a9b32ef969fc0b9db8bbf73f9384c7
Bradley Brown <tall.white.ninja@gmail.com>
2016-04-17 14:57:59 -0600

    more chapters gettin' dun

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,30 +1,30 @@
 import {Component} from 'angular2/core';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
+import {OnInit} from 'angular2/core';
 
 @Component({
 	selector: 'my-app',
 	styleUrls: ['styles.css'],
 	templateUrl: 'template.html',
-	directives: [HeroDetailComponent]
+	directives: [HeroDetailComponent],
+	providers: [HeroService]
 })
-export class AppComponent {
-	public heroes = HEROES;
+
+
+export class AppComponent implements OnInit {
+	constructor(private _heroService: HeroService) { }
+	getHeroes() {
+		this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+	}
+	heroes: Hero[];
 	title = "Heroes";
 	selectedHero: Hero;
 	onSelect(hero: Hero) {
 		this.selectedHero = hero;
 	}
+	ngOnInit() {
+		this.getHeroes();
+	}
 };
-var HEROES: Hero[] = [
-	{ "id": 11, "name": "Mr. Nice" },
-	{ "id": 12, "name": "Narco" },
-	{ "id": 13, "name": "Bombasto" },
-	{ "id": 14, "name": "Celeritas" },
-	{ "id": 15, "name": "Magneta" },
-	{ "id": 16, "name": "RubberMan" },
-	{ "id": 17, "name": "Dynama" },
-	{ "id": 18, "name": "Dr IQ" },
-	{ "id": 19, "name": "Magma" },
-	{ "id": 20, "name": "Tornado" }
-];
